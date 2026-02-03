486f2570dc98fe4d7845eca719217ec0c07576b5
kevinma21 <kevinpratama1201@gmail.com>
2026-02-03 16:22:21 +0700

    refactor footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,9 @@ export default function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
-        <div className="px-25 py-12.5 text-center text-gray-500">
-            <p>© <span>{currentYear}</span> Kevin Pratama. All rights reserved.</p>
+        <div className="md:px-25 py-12.5 text-sm text-center text-gray-500">
+            <p>© <span>{currentYear}</span> Kevin Pratama. <br/>
+            All rights reserved.</p>
         </div>
     );
 }
